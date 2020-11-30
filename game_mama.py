96a6024f11bd043eb4df1c181eaca982e0884a22
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2020-11-30 23:07:54 +0900

    게임 #15

--- game_mama.py
+++ game_mama.py
@@ -12,34 +12,31 @@ class Game_mama(QWidget):
 
     def initUI(self):
 
-        SCREEN_WIDTH = 480
+        screen_width = 480
         SCREEN_HEIGHT = 640
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # 화면 크기 설정
+        screen = pygame.display.set_mode((screen_width, SCREEN_HEIGHT))  # 화면 크기 설정
         clock = pygame.time.Clock()
 
         # 변수
-
         RED = (255, 0, 0)
         YELLOW = (255, 255, 0)
-        large_font = pygame.font.SysFont('malgungothic', 72)
-        small_font = pygame.font.SysFont('malgungothic', 36)
+        small_font = pygame.font.SysFont('malgungothic', 35)
         score = 0
-        missed = 0
+        shooted = 0
         game_over = False
 
         background_image = pygame.image.load("image/game_back.jpg")
-        rock_image = pygame.image.load("image/sandwich.png")
-        rocks = []
-        for i in range(3):
-            rock = rock_image.get_rect(left=random.randint(0, SCREEN_WIDTH - rock_image.get_width()), top=-100)
+
+        bad_image = pygame.image.load("image/sandwich.png")
+        bads = []
+
+        for i in range(4):
+            bad = bad_image.get_rect(left=random.randint(0, screen_width - bad_image.get_width()), top=-100)
             dy = random.randint(3, 9)
-            rocks.append((rock, dy))
+            bads.append((bad, dy))
 
-        missile_image = pygame.image.load("image/coffee.png")
-        missiles = []
-
-        fighter_image = pygame.image.load("image/game_baby.png")
-        fighter = fighter_image.get_rect(centerx=SCREEN_WIDTH // 2, bottom=SCREEN_HEIGHT)
+        fighter_image = pygame.image.load("image/game_baby.png") #도망 다니는 아이
+        fighter = fighter_image.get_rect(centerx=screen_width // 2, bottom=SCREEN_HEIGHT)
 
         # pygame.mixer.init()
         # pygame.mixer.music.load('music.mid') #배경 음악
@@ -47,7 +44,9 @@ class Game_mama(QWidget):
         # missile_sound = pygame.mixer.Sound('missile.wav') #사운드
         # explosion_sound = pygame.mixer.Sound('explosion.wav')
         # game_over_sound = pygame.mixer.Sound('game_over.wav')
-        running=True
+
+        running = True
+
         while running:  # 게임 루프
             screen.blit(background_image, (0, 0))
 
@@ -56,91 +55,62 @@ class Game_mama(QWidget):
             event = pygame.event.poll()  # 이벤트 처리
             if event.type == pygame.QUIT:
                 break
-            elif event.type == pygame.KEYDOWN and not game_over:
-                if event.key == pygame.K_SPACE:
-                    missile = missile_image.get_rect(centerx=fighter.centerx, top=fighter.top)
-                    missiles.append(missile)
 
-            pressed = pygame.key.get_pressed()
-            if pressed[pygame.K_LEFT] and not game_over:
+            pressed = pygame.key.get_pressed() # 화살표가 눌렸을 때 변수
+
+            if pressed[pygame.K_LEFT]: #왼쪽 키가 눌리고 게임이 끝나지 않
                 fighter.left -= 5
-            elif pressed[pygame.K_RIGHT] and not game_over:
+            elif pressed[pygame.K_RIGHT]:
                 fighter.left += 5
 
             if not game_over:
-                for rock, dy in rocks:
-                    rock.top += dy
-                    if rock.top > SCREEN_HEIGHT:
-                        rocks.remove((rock, dy))
-                        rock = rock_image.get_rect(left=random.randint(0, SCREEN_WIDTH - rock_image.get_width()),
+                for bad, dy in bads:
+                    bad.top += dy
+                    if bad.top > SCREEN_HEIGHT:
+                        bads.remove((bad, dy))
+                        bad = bad_image.get_rect(left=random.randint(0, screen_width - bad_image.get_width()),
                                                    top=-100)
                         dy = random.randint(3, 9)
-                        rocks.append((rock, dy))
-                        missed += 1
-
-                if missed >= 100:
-                    game_over = True
-                    pygame.mixer.music.stop()
-
-                for missile in missiles:
-                    missile.top -= 6
-                    if missile.top < 0:
-                        missiles.remove(missile)
+                        bads.append((bad, dy))
+                        shooted += 1
 
                 if fighter.left < 0:
                     fighter.left = 0
-                elif fighter.right > SCREEN_WIDTH:
-                    fighter.right = SCREEN_WIDTH
 
-                for rock, dy in rocks:
-                    for missile in missiles:
-                        if missile.colliderect(rock):
-                            # print('충돌')
-                            # print(rock)
-                            # print(missile)
-                            rocks.remove((rock, dy))
-                            missiles.remove(missile)
-                            rock = rock_image.get_rect(left=random.randint(0, SCREEN_WIDTH - rock_image.get_width()),
-                                                       top=-100)
-                            dy = random.randint(3, 9)
-                            rocks.append((rock, dy))
-                            score += 1
+                elif fighter.right > screen_width:
+                    fighter.right = screen_width
 
-                for rock, dy in rocks:
-                    if rock.colliderect(fighter):
+                for bad, dy in bads:
+                    if bad.colliderect(fighter):
                         # print('충돌')
-                        # print(rock)
+                        # print(bad)
                         # print(fighter)
-                        running=False
+                        running = False
                         pygame.mixer.music.stop()
 
             # 화면 그리기
 
-            for rock, dy in rocks:
-                screen.blit(rock_image, rock)
-
-            for missile in missiles:
-                screen.blit(missile_image, missile)
+            for bad, dy in bads:
+                screen.blit(bad_image, bad)
 
             screen.blit(fighter_image, fighter)
 
             score_image = small_font.render('점수 {}'.format(score), True, YELLOW)
             screen.blit(score_image, (10, 10))
 
-            # missed_image = small_font.render('놓친 운석 수 {}'.format(missed), True, YELLOW)
-            # screen.blit(missed_image, missed_image.get_rect(right=SCREEN_WIDTH - 10, top=10))
-
             if running==False:
-                game_over_image = large_font.render('게임 종료', True, RED)
-                screen.blit(game_over_image,
-                            game_over_image.get_rect(centerx=SCREEN_WIDTH // 2, centery=SCREEN_HEIGHT // 2))
+                game_over_image = small_font.render('게임 종료', True, RED)
+                game_over_image1 = small_font.render(' score : {}'.format(score), True, RED)
+                screen.blit(game_over_image, game_over_image.get_rect(centerx=screen_width // 2 , centery=SCREEN_HEIGHT // 2 - 50))
+                screen.blit(game_over_image1, game_over_image1.get_rect(centerx=screen_width // 2, centery=SCREEN_HEIGHT // 2))
 
-            pygame.display.update()  # 모든 화면 그리기 업데이트
-            clock.tick(60)  # 30 FPS (초당 프레임 수) 를 위한 딜레이 추가, 딜레이 시간이 아닌 목표로 하는 FPS 값
-        pygame.time.delay(2000)
+            pygame.display.update()  # 모든 화면 수시로 업데이트
+            clock.tick(60)  # 초당 프레임
+        pygame.time.delay(2000) #2초의 약간 딜레이
 
         pygame.quit()
-    def closeEvent(self, event):
+
+    def closeEvent(self, event): #종료 이벤트
         reply = QMessageBox.question(self, 'Message', 'Are you sure to quit?',
                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
